29ef6d250251423640a5ef962653b8963ccbb491
Ujjwal8271 <ujjwal.it.20183048@gmail.com>
2023-10-26 21:15:24 +0530

    assignment1

--- Assignmnet1/Assignment3/src/NonRepeating.java
+++ Assignmnet1/Assignment3/src/NonRepeating.java
@@ -6,16 +6,16 @@ public class NonRepeating {
 		for(int i=0;i<size;i++)
 		{
 			int count=0;
-			for(int j=i+1;j<size;j++)
+			for(int j=0;j<size;j++)
 			{
-				
+
 				if(arr[i]==arr[j])
 				{
-					count++;
-					break; 
+					count=count+1;
+					 
 				}
 			}
-			if(count==0)
+			if(count==1)
 			{
 		      return i;
 			}
@@ -27,7 +27,7 @@ public class NonRepeating {
 	public static void main(String[] args) {
 		
 
-		  int arr[]={ 1, 2, 3, -1, 2, 1, 0, 4, -1, 7, 8 };
+		  int arr[]={ 1, 2,  -1, 2, 1, 0, 4, -1, 7, 8 };
 		  int ans=Find(arr,arr.length);
 		  System.out.println(arr[ans]);
 		  
